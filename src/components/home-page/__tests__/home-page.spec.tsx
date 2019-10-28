41a8763c9d771a0e3c1721de0746bd74ce81c7d2
Paul Coroneos <paul@pcoroneos.com>
2019-10-27 21:10:17 -0500

    clean up package file. additional tooling

--- src/components/home-page/__tests__/home-page.spec.tsx
+++ src/components/home-page/__tests__/home-page.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { HomePage } from '..';
-import '@testing-library/jest-dom/extend-expect'
 
 describe('<HomePage />', () => {
   test('Expect header with text content Recent Blog Posts', () => {
