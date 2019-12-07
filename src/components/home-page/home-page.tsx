c94676ed527146af9eb88f16f2ed8761ef3832e3
Paul Coroneos <paul@pcoroneos.com>
2019-12-06 22:30:32 -0600

    12-06-19 blogpost. add some initial cypress files

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -8,6 +8,12 @@ import { Hero } from '../hero';
 
 const blogPosts = [
   {
+    header: 'Using caution with aria-selected and role="tab"',
+    date: '2019-12-06',
+    body:
+      ['As a front end developer one my weak areas is a11y. I was working a bug today at work involving an implementation that utilized tabs. Per my coworker in order for the feature to work I needed to utilize aria-selected and aria-controls. I implemented the feature and things were working great in Chrome with voiceover. However to my surprise and dissapointed it failed testing in Safari. Initially I was very confused as to how this was possible. Finally after about 15 minutes of debug I realized that I had placed the aria-select one level too high. Oddly enough chrome seems to automatically appends aria-selected=true when a tab has role="tab" and it is tab active. Safari makes no such behavior. I hope this helps you if you run into a similar situation!']
+  },
+  {
     header: 'Big trouble with parseInt()',
     date: '2019-11-28',
     body:
