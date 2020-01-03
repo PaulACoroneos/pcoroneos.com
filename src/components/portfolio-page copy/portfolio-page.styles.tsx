a46b3d63ef229fb797a4f201275e0360d1e63b0c
Paul Coroneos <paul@pcoroneos.com>
2020-01-03 01:33:20 -0600

    fix master

--- src/components/portfolio-page copy/portfolio-page.styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { rem } from 'polished';
-
-export const PortfolioWrapper = styled.section`
-  padding: ${rem('8px')};
-  margin: 0 auto;
-  max-width: 800px;
-`;
