bc4d2251fedee11e4598e0dd6f483aeef38cdd6f
Paul Coroneos <paul@pcoroneos.com>
2020-01-13 20:37:02 -0600

    initial aboutme implementation. WIP

--- src/components/about-page/about-page.styles.tsx
+++ src/components/about-page/about-page.styles.tsx
@@ -7,3 +7,39 @@ export const AboutWrapper = styled.section`
   max-width: 800px;
   height:100%;
 `;
+
+
+export const AboutMeContent = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  border: 5px solid ${props => props.theme.color.primary};
+  border-radius:5px;
+  background:${props => props.theme.color.white};
+  border-radius:10px;
+  padding:8px;
+  margin: 8px 0px;
+  font-size:18px;
+`;
+
+export const CaptionedImageContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+  border: 5px solid ${props => props.theme.color.primary};
+  border-radius:5px;
+  padding:8px;
+  max-width:450px;
+
+  img {
+    max-width:375px;
+    margin:8px;
+    align-self: center;
+  }
+
+  span {
+    font-size:16px;
+  }
+`;
