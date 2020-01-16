ef529b5ad47d5106344222f2c240c64271e8b806
Paul Coroneos <paul@pcoroneos.com>
2020-01-16 13:42:28 -0600

    correct stack post date

--- src/components/blog-posts/stack.tsx
+++ src/components/blog-posts/stack.tsx
@@ -11,7 +11,7 @@ const StyledImage = styled.img`
 
 export const Stack = () => <BlogpostWrapper>
   <h3>{'My first stack'}</h3>
-  <h4>{'Date Posted: 2019-01-15'}</h4>
+  <h4>{'Date Posted: 2020-01-15'}</h4>
   <p>{'For a long time i\'ve wanted to learn data structures and algorithms. I decided with the new year it was finally time to get started. After all the best time to start something is yesterday. The next best time is today.'}</p>
   <p>{'The first stop in learning data structures is stacks. You can technically use an array as the base of building a stack in JavaScript but I instead elected to start with an object. This is my version of the implementation:'}</p>
   <StyledImage alt='My implementation of a stack with explanation below' src='/stack.png' />
