0f38256e9edc7d05fb5ef081c0b158d7f3414515
Paul Coroneos <paul@pcoroneos.com>
2020-01-18 15:22:38 -0600

    remove content borders on blog and about page

--- src/components/about-page/about-page.styles.tsx
+++ src/components/about-page/about-page.styles.tsx
@@ -15,10 +15,7 @@ export const AboutMeContent = styled.div`
   flex-direction:column;
   justify-content:center;
   align-items:center;
-  border: 5px solid ${props => props.theme.color.primary};
-  border-radius:5px;
   background:${props => props.theme.color.white};
-  border-radius:10px;
   padding:8px;
   margin: 8px 0px;
   font-size:18px;
--- src/components/blog-posts/blog-posts.styles.tsx
+++ src/components/blog-posts/blog-posts.styles.tsx
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 
 export const BlogpostWrapper = styled.article`
     display:block;
-    border: 5px solid ${props => props.theme.color.primary};
-    border-radius:5px;
     background:${props => props.theme.color.white};
     border-radius:10px;
     padding:8px;
