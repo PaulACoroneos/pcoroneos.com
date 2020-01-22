1d69c03f7625b61c72210d5d210ade66abbffb77
Paul Coroneos <paul@pcoroneos.com>
2020-01-22 00:56:15 -0600

    add the ability to add images to blog posts including a fun caption

--- src/pages/hello-world.tsx
+++ src/pages/hello-world.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import BlogPost from '../components/blog-post/blog-post';
 
 const HelloWorld = () =>
-  <BlogPost header='Hello World' date='2019-08-19'>
+  <BlogPost header='Hello World' date='2019-08-19' src='hello.jpg' alt='A neon sign in the shape of the word hello' desc='I love neon signs'>
     <p>{'Hi there. My name is Paul. I am a fairly new web developer with a primary focus on front end. My background before becoming a full-time software dev was 7.5 years in the semiconductor industry as a test engineer.'}</p>
     <p>{'I\'ve been helped quite a bit by posts on the web as I have continued to improve my javascript accumen. So I want to give back and give everyone some perspective as I learn new and cool things. Check back as I make occasional posts on things I learn.'}</p>
     <p>{'Take care!'}</p>
