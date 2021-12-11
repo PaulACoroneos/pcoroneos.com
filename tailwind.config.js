fb6caa9c929846bb56bbaa54037124513ab5282f
Paul Coroneos <paul@pcoroneos.com>
2021-12-11 00:07:39 -0600

    mdx and tw updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,26 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const mdx = require('@mdx-js/mdx')
 
 module.exports = {
-  purge: {
-    mode: 'all',
-    content: ['./src/**/*.{js,ts,tsx,mdx}', './next.config.js'],
-    options: {
-      extractors: [
-        {
-          extensions: ['mdx'],
-          extractor: content => {
-            content = mdx.sync(content)
+  content: ['./src/**/*.{js,ts,tsx,mdx}'],
+  options: {
+    extractors: [
+      {
+        extensions: ['mdx'],
+        extractor: content => {
+          content = mdx.sync(content)
 
-            // Capture as liberally as possible, including things like `h-(screen-1.5)`
-            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+          // Capture as liberally as possible, including things like `h-(screen-1.5)`
+          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
 
-            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-            const innerMatches =
-              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+          // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+          const innerMatches =
+            content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
 
-            return broadMatches.concat(innerMatches)
-          },
+          return broadMatches.concat(innerMatches)
         },
-      ],
-    },
+      },
+    ],
   },
-  darkMode:'class',
   theme: {
     colors: {
       primary: 'var(--color-primary)',
@@ -44,7 +40,7 @@ module.exports = {
         blue: '#93ddfd',
         white: '#fff',
       },
-  },
+    },
     extend: {
       lineHeight: {
         '11': '2.75rem',
@@ -71,9 +67,8 @@ module.exports = {
       },
     },
   },
-  variants: {},
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-   ],
+  ],
 }
