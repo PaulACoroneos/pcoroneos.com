a14b0265794549e2d7a2912d7b8252f37caf43aa
Paul Coroneos <paul@pcoroneos.com>
2021-12-17 00:35:35 -0600

    kill rss for now

--- next.config.js
+++ next.config.js
@@ -104,11 +104,11 @@ module.exports = withBundleAnalyzer({
     if (!options.dev && options.isServer) {
       const originalEntry = config.entry
 
-      config.entry = async () => {
-        const entries = { ...await originalEntry() }
-        entries['./scripts/build-rss.js'] = './scripts/build-rss.js'
-        return entries
-      }
+      // config.entry = async () => {
+      //   const entries = { ...await originalEntry() }
+      //   entries['./scripts/build-rss.js'] = './scripts/build-rss.js'
+      //   return entries
+      // }
     }
 
     return config
