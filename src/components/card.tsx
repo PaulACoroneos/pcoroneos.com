5aa10342f36a3ff08121074f13e2163ac81667c3
Paul Coroneos <paul@pcoroneos.com>
2022-02-02 23:06:15 -0600

    fix sizing on deploy button of portfolio projects. add wip what do i wear on my run to portfolio page

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 export type CardProps = {
   title: string;
@@ -7,39 +7,34 @@ export type CardProps = {
   imageSrc: string;
 };
 
-const Card = ({
-  title,
-  githubLink,
-  deployedLink,
-  imageSrc,
-}: CardProps) => {
+const Card = ({ title, githubLink, deployedLink, imageSrc }: CardProps) => {
   const githubTitle = `Github link for ${title}`;
   const deployedTitle = `Deployed link for ${title}`;
 
   return (
-    <div className='flex flex-col items-center'>
-      <div className='bg-white rounded-lg mt-5'>
-        <img src={imageSrc} className='h-40 rounded-md' alt='' />
+    <div className="flex flex-col items-center">
+      <div className="bg-white rounded-lg mt-5">
+        <img src={imageSrc} className="h-40 rounded-md" alt="" />
       </div>
-      <div className='bg-secondary shadow-lg rounded-lg -mt-4 w-64 h-56 flex flex-col justify-between'>
-        <div className='py-5 px-5'>
-          <span className='font-bold text-lg text-white'>{title}</span>
+      <div className="bg-secondary shadow-lg rounded-lg -mt-4 w-64 h-56 flex flex-col justify-between">
+        <div className="py-5 px-5">
+          <span className="font-bold text-lg text-white">{title}</span>
         </div>
-        <div className='p-4 grid grid-cols-2 gap-1'>
+        <div className="p-4 grid grid-cols-2 gap-1">
           <a title={githubTitle} href={githubLink}>
-            <div className='px-2 py-1 grid grid-cols-2 items-center bg-primary rounded'>
-              <img src='./github.png' alt='' />
-              <span className='text-white text-sm'>Github</span>
+            <div className="px-2 py-1 grid grid-cols-2 items-center bg-primary rounded">
+              <img src="./github.png" alt="" />
+              <span className="text-white text-sm">Github</span>
             </div>
           </a>
-          {deployedLink &&
+          {deployedLink && (
             <a title={deployedTitle} href={deployedLink}>
-              <div className='px-2 py-1 grid grid-cols-2 items-center bg-primary rounded'>
-                <img width='40' height='40' src='./deploy.png' alt='' />
-                <span className='text-white text-sm'>Deploy</span>
+              <div className="px-2 py-1 grid grid-cols-2 items-center bg-primary rounded">
+                <img width="39" height="39" src="./deploy.png" alt="" />
+                <span className="text-white text-sm">Deploy</span>
               </div>
             </a>
-          }
+          )}
         </div>
       </div>
     </div>
