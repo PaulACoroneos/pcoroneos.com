2698ca081f3e9ab5be8eaa564a306e249184b758
Paul Coroneos <paul.coroneos@icloud.com>
2022-10-27 00:49:59 -0500

    migrate to all the latest packages

--- getStaticProps.ts
+++ getStaticProps.ts
@@ -1,11 +1,14 @@
 import getAllPostPreviews from './getAllPostPreviews'
 
 export async function getStaticProps() {
+
+  const posts = getAllPostPreviews();
+
   return {
     props: {
-      posts: getAllPostPreviews().map(post => ({
-        title: post.module.meta.title,
-        link: post.link,
+      posts: posts?.map(post => ({
+        title: post?.module?.meta?.title ?? '',
+        link: post?.link ?? '',
       })),
     },
   }
---
+++ scripts/preview.js
@@ -0,0 +1,9 @@
+module.exports = function (src) {
+  if (src.includes('{/* more */}')) {
+    const [preview] = src.split('{/* more */}')
+    return this.callback(null, preview)
+  }
+
+  const [preview] = src.split('{/* /excerpt */}')
+  return this.callback(null, preview.replace('{/* excerpt */}', ''))
+}
