0aa5122b59cf4c44cadd7e9afa91f113bfa3e0cd
Paul Coroneos <paul.coroneos@icloud.com>
2022-11-13 22:01:14 -0600

    use invisible rather than display hidden for now

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => <footer className='py-4 flex justify-between items-center'>
     <a className='text-secondary hover:text-primary' href='https://twitter.com/pacman326'>Twitter</a>
     {' '}
     <a className='text-secondary hover:text-primary' href='https://www.linkedin.com/in/paulacoroneos/'>LinkedIn</a>
-    <a rel='me' className='hidden' href='https://mstdn.social/@pacman326' />
+    <a rel='me' className='invisible' href='https://mstdn.social/@pacman326' />
   </div>
 </footer>
 
