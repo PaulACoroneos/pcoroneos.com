d7a2ca53565fe025575c962c62c444d6d300a239
Paul Coroneos <paul.coroneos@icloud.com>
2023-05-20 02:50:25 -0500

    fixes

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
+import * as React from "react";
 
-const PageTitle = ({ children }) => 
-    <h1 className='text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14'>
-      {children}
-    </h1>
-  
+const PageTitle = ({ children }: { children: React.ReactNode }) => (
+  <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    {children}
+  </h1>
+);
 
-export default PageTitle
+export default PageTitle;
