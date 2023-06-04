d93560622f2850af554b146c1040e2c6f9cd52b6
Paul Coroneos <paul.coroneos@icloud.com>
2023-06-03 19:33:59 -0500

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -50,7 +50,7 @@ export default withMDX({
 
       config.entry = async () => {
         const entries = { ...(await originalEntry()) };
-        entries["./scripts/build-rss.js"] = "./scripts/build-rss";
+        entries["./scripts/build-rss"] = "./scripts/build-rss";
         return entries;
       };
     }
