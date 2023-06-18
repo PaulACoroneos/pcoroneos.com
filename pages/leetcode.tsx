1f6911e77731a3e23504612e041214ce04b1c731
Paul Coroneos <paul.coroneos@icloud.com>
2023-06-18 15:42:41 -0500

    implement simple search on leetcode page (#130)

--- pages/leetcode.tsx
+++ pages/leetcode.tsx
@@ -5,12 +5,24 @@ import { format, parseISO } from "date-fns";
 import getAllPostPreviews from "getAllPostPreviews";
 
 const Leetcode = () => {
-  const sortedPostsInDesc = getAllPostPreviews()
-    .sort((a, b) =>
-      new Date(b.module.meta.date) > new Date(a.module.meta.date) ? 1 : -1
-    )
-    .filter((post) => post?.module?.meta?.date)
-    .filter((post) => post.module?.meta?.tags?.includes("leetcode"));
+  const [typedFilter, setTypedFilter] = React.useState<string | null>(null);
+
+  const sortedPostsInDesc = React.useMemo(
+    () =>
+      getAllPostPreviews()
+        .sort((a, b) =>
+          new Date(b.module.meta.date) > new Date(a.module.meta.date) ? 1 : -1
+        )
+        .filter((post) => post?.module?.meta?.date)
+        .filter((post) => post.module?.meta?.tags?.includes("leetcode")),
+    []
+  );
+
+  const posts = typedFilter
+    ? sortedPostsInDesc.filter((post) =>
+        post.module?.meta?.title.includes(typedFilter)
+      )
+    : sortedPostsInDesc;
 
   return (
     <div className="divide-gray-200 divide-y">
@@ -45,44 +57,52 @@ const Leetcode = () => {
           Latest Leetcode Solutions
         </h1>
       </div>
+      <div className="flex justify-end py-4">
+        <label>
+          <span className="pr-2 text-lg font-medium">Search Solutions</span>
+          <input
+            className="appearance-none rounded border pl-2 shadow"
+            type="text"
+            onKeyUp={(evt) => setTypedFilter(evt.currentTarget.value)}
+          />
+        </label>
+      </div>
       <ul className="divide-gray-200 divide-y">
-        {sortedPostsInDesc.map(
-          ({ link, module: { default: Component, meta } }) => (
-            <li key={link} className="py-4 md:py-12">
-              <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                <dl>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-gray-500 text-base font-medium leading-6">
-                    <time dateTime={meta.date}>
-                      {format(parseISO(meta.date), "MMMM dd, yyyy")}
-                    </time>
-                  </dd>
-                </dl>
-                <div className="space-y-5 xl:col-span-3">
-                  <div className="space-y-6">
-                    <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                      <Link href={link} className="text-gray-900">
-                        {meta.title}
-                      </Link>
-                    </h2>
-                    <div className="text-gray-500 prose">
-                      <Component />
-                    </div>
-                  </div>
-                  <div className="text-base font-medium leading-6">
-                    <Link
-                      href={link}
-                      className="text-secondary hover:text-primary"
-                      aria-label={`Read "${meta.title}"`}
-                    >
-                      Read more &rarr;
+        {posts.map(({ link, module: { default: Component, meta } }) => (
+          <li key={link} className="py-4 md:py-12">
+            <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+              <dl>
+                <dt className="sr-only">Published on</dt>
+                <dd className="text-gray-500 text-base font-medium leading-6">
+                  <time dateTime={meta.date}>
+                    {format(parseISO(meta.date), "MMMM dd, yyyy")}
+                  </time>
+                </dd>
+              </dl>
+              <div className="space-y-5 xl:col-span-3">
+                <div className="space-y-6">
+                  <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                    <Link href={link} className="text-gray-900">
+                      {meta.title}
                     </Link>
+                  </h2>
+                  <div className="text-gray-500 prose">
+                    <Component />
                   </div>
                 </div>
-              </article>
-            </li>
-          )
-        )}
+                <div className="text-base font-medium leading-6">
+                  <Link
+                    href={link}
+                    className="text-secondary hover:text-primary"
+                    aria-label={`Read "${meta.title}"`}
+                  >
+                    Read more &rarr;
+                  </Link>
+                </div>
+              </div>
+            </article>
+          </li>
+        ))}
       </ul>
     </div>
   );
