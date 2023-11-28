845b2cdd5200f5edc64c7d0da8628c4aa59c269c
Paul Coroneos <paul.coroneos@icloud.com>
2023-11-27 22:29:55 -0600

    add project to portfolio (#139)

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -2,23 +2,14 @@ import Card from "components/card";
 
 const data = [
   {
-    title: "Free Code Camp Scientific Computing With Python Projects",
-    githubLink:
-      "https://github.com/PaulACoroneos/Scientific-Computing-with-Python-Projects",
-    imageSrc: "./fcc.png",
-    deployedLink:
-      "https://repl.it/repls/folder/Scientific-Computing-with-Python-Projects",
-  },
-  {
-    title: "What do I wear on my run?! (WIP)",
-    deployedLink: "https://what-do-i-wear-on-my-run.vercel.app",
-    githubLink: "https://github.com/PaulACoroneos/What-Do-I-Wear-On-My-Run",
-    imageSrc: "./dude-what-do-i-wear-on-my-run.jpg",
+    title: "Vanilla HTML and CSS Survey Form",
+    githubLink: "https://github.com/PaulACoroneos/survey-form",
+    deployedLink: "https://paulacoroneos.github.io/survey-form/",
   },
 ];
 
 const Portfolio = () => (
-  <ul className="grid gap-8 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+  <ul className="grid min-h-[800px] gap-8 pt-6 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
     {data.map((cardData) => (
       <li key={cardData.title}>
         <Card {...cardData} />
