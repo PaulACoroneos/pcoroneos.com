a32f19787bebbf0964c5b1d3dfb82ffb9df4165b
Paul Coroneos <paul.coroneos@icloud.com>
2023-12-21 00:03:41 -0600

    portfolio project card refactor (#144)

--- components/section-container.tsx
+++ components/section-container.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 
 const SectionContainer = ({ children }: { children: React.ReactNode }) => (
-  <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
-    {children}
-  </div>
+  <div className="mx-auto max-w-3xl px-2 xl:max-w-5xl">{children}</div>
 );
 
 export default SectionContainer;
