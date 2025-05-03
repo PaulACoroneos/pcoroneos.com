dfee25eb5d2efa2a54ccde0dfe65ee668c1da45d
pcoroneos <paul.coroneos@icloud.com>
2025-05-03 01:59:44 -0500

    344 leetcode reverse string

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    watch: false,
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setup.js",
